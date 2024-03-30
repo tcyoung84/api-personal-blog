8483457cf1f074a7e905d09cdb2bf6b5642fd679
TCY <tcyoung.84@gmail.com>
2024-03-30 16:37:28 -0400

    began blog.js

---
+++ assets/js/blog.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleModeButton = document.getElementById('toggleMode');
+    const blogPostsContainer = document.getElementById('blogPosts');
